bb4a22b9197d7a1c8fb58953059e82a9e3fa49a3
craftguruu-resin <craftguruu@gmail.com>
2026-09-23 14:02:21 +0530

    Show order discount in bill breakdown

--- vendororder.js
+++ vendororder.js
@@ -192,6 +192,7 @@
        "<div>Product value " +
        money(totals.productValue != null ? totals.productValue : totals.subtotal)
       "</div>" +
+       (Number(totals.prepaidDiscount) > 0 ? "<div>Online discount −" + money(totals.prepaidDiscount) + "</div>" : "") +
       "<div>Shipping " +
       money(totals.shipping) +
       "</div>" +
@@ -270,6 +271,7 @@
       "<div><strong>Product value</strong> " +
        money(totals.productValue != null ? totals.productValue : totals.subtotal)
       "</div>" +
+       (Number(totals.prepaidDiscount) > 0 ? "<div><strong>Online discount</strong> −" + money(totals.prepaidDiscount) + "</div>" : "") +
       "<div><strong>Shipping</strong> " +
       money(totals.shipping) +
       "</div>" +
